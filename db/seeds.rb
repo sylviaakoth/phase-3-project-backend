2176395c096b3d431e1e9c2c6e5768044997dc08
Omari-Elizabeth <elizabeth.omari@student.moringaschool.com>
2022-11-09 13:47:37 +0300

    seed doctors table

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,11 @@
 puts "🌱 Seeding spices..."
 
 # Seed your database here
+10.times do
+    doctor = Doctor.create(
+        name: Faker::Name.name, 
+        contact: Faker::Contact.contact
+    )
+end
 
 puts "✅ Done seeding!"
