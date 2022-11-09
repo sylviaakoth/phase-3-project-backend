7390a6f9c6aa4348946c77948b2741b3a85e0d42
Dan Muendo <danwalker254254@gmail.com>
2022-11-09 13:09:02 -0500

    updated files

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,22 +1,38 @@
 puts "🌱 Seeding spices..."
 
 # Seed your database here
+puts "Deleting old data..."
+Patient.destroy_all
+Doctor.destroy_all
+Appointment.destroy_all
+Specialist.destroy_all
+
+puts "Done deleting"
+
+# SET @num := 0;
+# UPDATE your_table SET id = @num := (@num+1);
+# ALTER TABLE tableName AUTO_INCREMENT = 1;
+
 10.times do
     doctor = Doctor.create(
         name: Faker::Name.name, 
-        contact: Faker::Contact.contact
+        contact: Faker::Internet.email
+        # speciality_id: specialist.id,
+        # appointment_id: appointment.id
     )
     patient = Patient.create(
         first_name: Faker::Name.first_name,
         last_name: Faker::Name.last_name,
-        gender: Faker::Gender.gender,
-        dob: Faker::Time.date
+        gender: Faker::Gender.binary_type,
+        age: rand(10..50),
+        # appointment_id: Appointment.id
     )
     appointment = Appointment.create(
         patients_id: patient.id,
         doctors_id: doctor.id
     )
 end
+
 
 Specialist.create(speciality: "dentist")
 Specialist.create(speciality: "pediatrician")
