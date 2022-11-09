fbf419f26dbda333272f4c729edbfcc73ffa507b
Omari-Elizabeth <elizabeth.omari@student.moringaschool.com>
2022-11-09 13:40:22 +0300

    create specialists table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_09_103409) do
+ActiveRecord::Schema.define(version: 2022_11_09_103920) do
 
   create_table "doctors", force: :cascade do |t|
     t.string "name"
@@ -27,6 +27,12 @@ ActiveRecord::Schema.define(version: 2022_11_09_103409) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "appointment_id"
+  end
+
+  create_table "specialists", force: :cascade do |t|
+    t.string "speciality"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
