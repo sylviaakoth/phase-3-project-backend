9e4441dae9048d84eb636b8e89665dca882ba0cd
Dan Muendo <danwalker254254@gmail.com>
2022-11-12 09:20:38 -0500

    updated files

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
+
+
   get "/doctors" do
     doctors = Doctor.all
     doctors.to_json
@@ -9,7 +11,7 @@ class ApplicationController < Sinatra::Base
 
   get "/patients" do
     doctors = Patient.all
-    doctors.to_json
+    doctors.to_json(include: :doctors)
   end
 
   get "/patients/:id" do
@@ -19,26 +21,45 @@ class ApplicationController < Sinatra::Base
 
   get "/doctors/:id" do
     patients = Doctor.find(params[:id])
-    patients.to_json(include: :specialists)
+    patients.to_json
   end
 
-
-
-  # delete "/appointments/:id" do
-  #   appointments = Doctor.find(params[:id])
-  #   appointments.destroy
-  #   appointments.to_json
-  # end
-  
+  delete "/patients/:id" do
+    patients = Patient.find(params[:id])
+    patients.destroy
+    patients.to_json
+  end
 
   post "/patients" do
     patient = Patient.create(
+      first_name: params[:firstName],
+      last_name: params[:lastName],
+      age: params[:age],
+      gender: params[:gender]
+    )
+    doctor = Doctor.all.sample
+
+    Appointment.create(
+      patient_id: patient.id,
+      doctor_id: doctor.id
+    )
+
+    patient.to_json
+  end
+
+  get "/appointments" do
+    appointments = Appointment.all
+    appointments.to_json
+  end
+
+  patch '/patients/:id' do
+    patient = Patient.find(params[:id])
+    patient.update(
       first_name: params[:first_name],
       last_name: params[:last_name],
       age: params[:age],
       gender: params[:gender]
     )
-
     patient.to_json
   end
   
